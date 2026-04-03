412d06a6b75541bfe9c8734854baf263820a3939
emre.wit.egitim@gmail.com <emre.wit.egitim@gmail.com>
2026-04-03 07:48:52 +0000

    feat(cc_parcel_product): add parcel_product mart table

---
+++ models/intermediate/int_cc_parcel_product.sql
@@ -0,0 +1,20 @@
+select
+    p.parcel_id,
+    pp.model_mame,
+    p.parcel_tracking,
+    p.transporter,
+    p.priority,
+    p.date_purchase,
+    p.date_shipping,
+    p.date_delivery,
+    p.date_cancelled,
+    p.month_purchase,
+    p.status,
+    p.expedition_time,
+    p.transport_time,
+    p.delivery_time,
+    p.delay,
+    pp.quantity as qty
+FROM {{ ref("int_cc_parcel")}} as p
+JOIN {{ ref("stg_raw__parcel_product")}} as pp
+    on p.parcel_id = pp.parcel_id
